be11973764d77e1a5192b05a4a5330d5a9a61f50
Kamil Stasiak <kamilstasiaknew@gmail.com>
2023-07-04 18:59:23 +0200

    Remove cors_plug

--- mix.exs
+++ mix.exs
@@ -39,7 +39,6 @@ defmodule Videoroom.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:cors_plug, "~> 3.0"},
 
       # Jellyfish deps
       {:jellyfish_server_sdk, github: "jellyfish-dev/server_sdk_elixir"},
