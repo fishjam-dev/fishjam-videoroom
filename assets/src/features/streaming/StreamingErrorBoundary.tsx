f1d0eb9c93b03666e5618dee96affcef85f30e2a
Kamil Stasiak <kamilstasiaknew@gmail.com>
2024-06-26 16:06:11 +0200

    Reconnect on network change (#81)

--- assets/src/features/streaming/StreamingErrorBoundary.tsx
+++ assets/src/features/streaming/StreamingErrorBoundary.tsx
@@ -1,11 +1,14 @@
 import { FC, PropsWithChildren, useCallback, useEffect, useState } from "react";
 import useToast from "../shared/hooks/useToast";
 import { ErrorMessage, messageComparator } from "../../pages/room/errorMessage";
-import { useClient } from "../../fishjam";
+import { PeerMetadata, TrackMetadata, useClient } from "../../fishjam";
 import useEffectOnChange from "../shared/hooks/useEffectOnChange";
+import { ClientEvents } from "@fishjam-dev/react-client";
+
+const RECONNECTING_TOAST_ID = "RECONNECTING";
 
 export const StreamingErrorBoundary: FC<PropsWithChildren> = ({ children }) => {
-  const { addToast } = useToast();
+  const { addToast, removeToast } = useToast();
 
   // todo remove state, refactor to function invocation
   const [errorMessage, setErrorMessage] = useState<ErrorMessage | undefined>();
@@ -20,26 +23,92 @@ export const StreamingErrorBoundary: FC<PropsWithChildren> = ({ children }) => {
     [setErrorMessage]
   );
 
+  const displayInfo = useCallback(
+    (text: string, id?: string) => {
+      addToast({
+        id: id || crypto.randomUUID(),
+        message: text,
+        timeout: 5000,
+        type: "information"
+      });
+    },
+    []
+  );
+
   useEffect(() => {
     if (!client) return;
 
-    const onSocketError = (_: Event) => {
+    const onReconnectionStarted: ClientEvents<PeerMetadata, TrackMetadata>["reconnectionStarted"] = () => {
+      console.log("%c" + "reconnectionStarted", "color:green");
+
+      addToast({
+        id: RECONNECTING_TOAST_ID,
+        message: "Reconnecting",
+        timeout: "INFINITY",
+        type: "information"
+      });
+    };
+
+    const onReconnected: ClientEvents<PeerMetadata, TrackMetadata>["reconnected"] = () => {
+      console.log("%cReconnected", "color:green");
+      removeToast(RECONNECTING_TOAST_ID);
+
+      setTimeout(() => {
+        displayInfo("Reconnected");
+      }, 2000);
+    };
+
+    const onReconnectionRetriesLimitReached: ClientEvents<PeerMetadata, TrackMetadata>["reconnectionRetriesLimitReached"] = () => {
+      console.log("%cReconnectionFailed", "color:red");
+      removeToast(RECONNECTING_TOAST_ID);
+
+      setTimeout(() => {
+        handleError(`Reconnection failed`);
+      }, 2000);
+    };
+
+    const onSocketError: ClientEvents<PeerMetadata, TrackMetadata>["socketError"] = (error: Event) => {
+      console.warn(error);
       handleError(`Socket error occurred.`, "onSocketError");
     };
 
-    const onConnectionError = (message: string) => {
-      handleError(`Connection error occurred. ${message ?? ""}`);
-    };
-    const onJoinError = (_: unknown) => {
-      handleError(`Failed to join the room`);
-    };
-    const onAuthError = () => {
-      handleError(`Socket error occurred.`, "onAuthError");
+    const onConnectionError: ClientEvents<PeerMetadata, TrackMetadata>["connectionError"] = (error, client) => {
+      if (client.isReconnecting()) {
+        console.log("%c" + "During reconnection: connectionError %o", "color:gray", {
+          error,
+          iceConnectionState: error?.event?.target?.["iceConnectionState"]
+        });
+      } else {
+        console.warn({ error, state: error?.event?.target?.["iceConnectionState"] });
+        handleError(`Connection error occurred. ${error?.message ?? ""}`);
+      }
     };
 
-    const onSocketClose = (_: Event) => {
-      handleError(`Signaling socket closed.`, "onSocketClose");
+    const onJoinError: ClientEvents<PeerMetadata, TrackMetadata>["joinError"] = (event) => {
+      console.log(event);
+      handleError(`Failed to join the room`);
     };
+
+    const onAuthError: ClientEvents<PeerMetadata, TrackMetadata>["authError"] = (reason) => {
+      if (client.isReconnecting()) {
+        console.log("%c" + "During reconnection: authError: " + reason, "color:gray");
+      } else {
+        handleError(`Socket error occurred.`, "onAuthError");
+      }
+    };
+
+    const onSocketClose: ClientEvents<PeerMetadata, TrackMetadata>["socketClose"] = (event) => {
+      if (client.isReconnecting()) {
+        console.log("%c" + "During reconnection: Signaling socket closed", "color:gray");
+      } else {
+        console.warn(event);
+        handleError(`Signaling socket closed.`, "onSocketClose");
+      }
+    };
+
+    client.on("reconnectionStarted", onReconnectionStarted);
+    client.on("reconnected", onReconnected);
+    client.on("reconnectionRetriesLimitReached", onReconnectionRetriesLimitReached);
 
     client.on("socketError", onSocketError);
     client.on("connectionError", onConnectionError);
@@ -48,6 +117,10 @@ export const StreamingErrorBoundary: FC<PropsWithChildren> = ({ children }) => {
     client.on("socketClose", onSocketClose);
 
     return () => {
+      client.off("reconnectionStarted", onReconnectionStarted);
+      client.off("reconnected", onReconnected);
+      client.off("reconnectionRetriesLimitReached", onReconnectionRetriesLimitReached);
+
       client.off("socketError", onSocketError);
       client.off("connectionError", onConnectionError);
       client.off("joinError", onJoinError);
