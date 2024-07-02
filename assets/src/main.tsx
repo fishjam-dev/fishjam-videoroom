cb7e0751895df7f55a3890dc2513b2d7ff47036a
Radosław Szuma <radoslawszuma@gmail.com>
2024-07-02 12:54:29 +0200

    Reorganize observability containers

--- assets/src/main.tsx
+++ assets/src/main.tsx
@@ -2,6 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import {
+  getWebInstrumentations,
+  initializeFaro,
+} from '@grafana/faro-web-sdk';
+
+const faro = initializeFaro({
+  url: 'http://localhost:8027/collect',
+  app: {
+    name: 'Your App Name',
+    version: '1.0.0',
+  },
+  instrumentations: [
+    ...getWebInstrumentations({
+      captureConsole: true,
+    }),
+  ]
+});
+
+faro.api.pushLog(["Example logs"])
+
+faro.api.pushMeasurement({
+  type: "test_measurement",
+  values: {
+    random_number: 120,
+    random_float: 0.5
+  }
+},
+  {
+    context: {
+      hello: 'world'
+    }
+  }
+)
+
+faro.api.pushEvent('Example event')
+
+
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
